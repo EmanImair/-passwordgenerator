d8943b71cf69bbf7969c77c77a409d93826dd70a
EmanImair <102223375+EmanImair@users.noreply.github.com>
2022-12-25 15:10:45 +0300

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -1,26 +1,34 @@
-#Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+           'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-password=[[letters] , [numbers] , [symbols]]
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
 password_list=[]
 
-for char in range(0,nr_letters):
+print("Welcome to the PyPassword Generator!")
+n_letters= int(input("how many letters would you like to have in your password : "))
+n_numbers=int(input("how many numbers would you like to have in your password :"))
+n_symbols=int(input("how many numbers would you like to have in your password :"))
+
+
+for char in range(0,n_letters):
   password_list.append(random.choice(letters))
-for char in range(0,nr_symbols):
-  password_list.append(random.choice(symbols))
-for char in range(0,nr_numbers):
+
+for char in range(0,n_numbers):
   password_list.append(random.choice(numbers))
+
+for char in range(0,n_symbols):
+  password_list.appened(random.choice(symbols))
 
 random.shuffle(password_list)
 
 password=""
+
 for char in password_list:
   password+=char
 
-print(f"password : {password}")
+
+print(f"password {password}")
